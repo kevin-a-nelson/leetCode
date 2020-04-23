a1a2cea5d01c4d1563b547d9e43e2bc708ff688a
SamuraiViking <nelson67@stolaf.edu>
2020-04-22 22:13:44 -0500

    addTwoNumbers

---
+++ addTwoNumbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        node = ListNode(0)
+        
+        head = node
+        
+        carry = 0
+        
+        while l1 or l2 or carry > 0:
+            
+            l1_val = (l1.val if l1 else 0)
+            l2_val = (l2.val if l2 else 0)
+            
+            sum = l1_val + l2_val + carry
+            remainder = sum % 10
+            carry = sum // 10
+            
+            node.next = ListNode(remainder)
+            
+            node = node.next
+            
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+        
+        return head.next
+    
--- test.py
+++ test.py
@@ -1,1 +1,46 @@
-HELOOOO
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def addTwoNumbers(l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    result = ListNode(0)
+    result_tail = result
+    carry = 0
+            
+    while l1 or l2 or carry:            
+        val1  = (l1.val if l1 else 0)
+        val2  = (l2.val if l2 else 0)
+        carry, out = divmod(val1+val2 + carry, 10)    
+                    
+        result_tail.next = ListNode(out)
+        result_tail = result_tail.next                      
+        
+        l1 = (l1.next if l1 else None)
+        l2 = (l2.next if l2 else None)
+            
+    return result.next
+
+def createListNode(arr):
+    head = ListNode(arr[0])
+    arr = arr[1:]
+    node = head
+    for val in arr:
+        node.next = ListNode(val)
+        node = node.next
+    return head
+
+l1 = createListNode([2, 4, 3])
+l2 = createListNode([5, 6, 4])
+
+l3 = addTwoNumbers(l1, l2)
+
+
+        
