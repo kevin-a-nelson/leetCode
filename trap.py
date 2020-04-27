996c2e38d52c43909c6945b3c2dbcc08977373c7
SamuraiViking <nelson67@stolaf.edu>
2020-04-26 22:50:42 -0500

    add

---
+++ trap.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trap(self, heights: List[int]) -> int:
+        
+        # leftPointer
+        # rightPointer
+        
+        # leftMax
+        # rightMax
+        
+        
+        # if heights[left] 
