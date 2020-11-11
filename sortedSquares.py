cf9fd8ebc20247d647aa946299503ca56e2e9ee9
Kevin Nelson <nelsonkevin767@gmail.com>
2020-11-11 17:15:40 -0600

    sortedSquares

--- sortedSquares.py
+++ sortedSquares.py
@@ -1,16 +1,36 @@
 class Solution(object):
     def sortedSquares(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
         
-        squaredA = []
-        for num in A:
-            squaredA.append(num ** 2)
-        
+        N = len(A)
+        j = 0
 
-        return sorted(squaredA)
-
-            
         
+        arr = []
+
+
+        while j < N and A[j] < 0:
+            j += 1
+        i = j - 1
+
+        while i >= 0 and j < N:
+            if A[i] ** 2 < A[j] ** 2:
+                arr.append(A[i] ** 2)
+                i -= 1
+            else:
+                arr.append(A[j] ** 2)
+                j += 1
+        
+        while i >= 0:
+            arr.append(A[i] ** 2)
+            i -= 1
+
+        while j < N:
+            arr.append(A[j] ** 2)
+            j += 1
+        
+        return arr
+                
+
+
+solution = Solution().sortedSquares([-4,-1,0,3,10])
+print(solution)
